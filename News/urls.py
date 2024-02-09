48dbddeaad5d7ac98ffad8865425cf6c2a3dc355
MushvigShukurov <musfiqsukurov9@gmail.com>
2024-02-10 02:05:25 +0400

    10.02.2024

--- News/urls.py
+++ News/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
 
-from News.views import NewsDetail
+from News.views import NewsDetail, AllNews
 
 urlpatterns = [
+    path('', AllNews, name="all-news"),
     path('detail/<str:news>', NewsDetail, name="news-detail"),
 ]
